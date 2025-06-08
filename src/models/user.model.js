f251caa5f90d17480a8c6564c53727fe6f85eef9
payal priya <payalpriya8360@gmail.com>
2025-06-08 08:35:50 +0530

    add

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -49,4 +49,20 @@ const userSchema = new Schema({
     },
 },{timestamps: true})
 
+userSchema.pre("save",async function(next){
+        if(!this.isModified("password")){
+            return next();
+        }
+        
+        // Hash the password before saving
+     this.password = await bcrypt.hash(this.password, 10);
+        next(); 
+
+})
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+
+
 export const User= mongoose.model("User", userSchema)
