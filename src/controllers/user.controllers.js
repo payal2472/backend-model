c803b088e4033d724523bc75322992cf58fad04a
payal priya <payalpriya8360@gmail.com>
2025-06-13 14:08:04 +0530

    avtar

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,4 +1,7 @@
 import asyncHandler from '../utils/asyncHandler.js';
+import apiError from '../utils/apiError.js';
+import {User} from '../models/user.model.js';
+import {uploadOnCloudinary} from '../utils/cloudinary.js';
 
 const registerUser = asyncHandler(async (req, res) => {
     
@@ -13,12 +16,35 @@ const registerUser = asyncHandler(async (req, res) => {
     // return response
     // res.status(201).json({ message: 'User registered successfully' });
 
-    const {fullName, username, email, password, avatar } = req.body;
+    const {fullName, username, email, password} = req.body;
     console.log("email", email);   
-
-
-
      
+    if ([fullName, username, email, password].some((field) => field?.trim()=== "")) 
+    
+    {
+         throw new apiError(400, "All fields are required");
+    }
+
+    const existedUser = User.findOne({
+        $or:[{username}, {email}]
+    })
+
+    if (existedUser) {
+        throw new apiError(409, "Username or email already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new apiError(400, "Avatar and cover image are required");
+        
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+     
+
+   
 });
 
 export {registerUser}; 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,11 +11,25 @@
 // routes/user.routes.js
 import { Router } from 'express';
 import { registerUser } from '../controllers/user.controllers.js';
+import {upload} from '../middlewares/multer.middlewares.js'; // import multer middleware if needed
 
 const router = Router(); // create router instance
 
 router.route('/register') //  define the route
-    .post(registerUser);  // bind controller
+    .post(
+        
+        upload.fields([
+            {
+                name: 'avatar', // field name for avatar
+                maxCount: 1 // limit to one file for avatar
+            },
+            {
+                name: 'coverImages', // field name for images
+                maxCount: 1 // limit to five files for images
+            },
+            {}
+        ]), // use multer middleware for file uploads
+        registerUser);  // bind controller
 
 export default router;    // export the instance
 
