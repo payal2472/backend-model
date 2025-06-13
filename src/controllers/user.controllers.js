54911ef8ba8ab24d2e6a3a6f5b63572f487768d2
payal priya <payalpriya8360@gmail.com>
2025-06-13 14:26:32 +0530

    regestration controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -2,6 +2,7 @@ import asyncHandler from '../utils/asyncHandler.js';
 import apiError from '../utils/apiError.js';
 import {User} from '../models/user.model.js';
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
+import {ApiResponse} from '../utils/ApiResponse.js';
 
 const registerUser = asyncHandler(async (req, res) => {
     
@@ -42,6 +43,28 @@ const registerUser = asyncHandler(async (req, res) => {
     }
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
+         throw new apiError(400, "Avatar file is required");
+        
+    }
+     const user = await User.create ({
+        fullName,
+        username: username.toLowerCase(),
+        email,
+        password,
+        avatar: avatar?.secure_url, // Use the secure URL from Cloudinary
+        coverImage: coverImage?.url || "", // Use the secure URL from Cloudinary
+        
+    })
+    const createdUser = await User.findById(user._id).select("-password -refreshToken"); 
+    if (!createdUser) {
+        throw new apiError(500, "somthng went wrong, user not created");
+        
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201, createdUser, "User registered successfully"));
      
 
    
